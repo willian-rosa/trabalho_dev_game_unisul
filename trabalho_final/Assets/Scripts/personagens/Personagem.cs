78d5d2af6d6f27dd05d1f048cce48210eb2fff71
Willian da Rosa <will.rosa.gvt@gmail.com>
2019-06-08 23:09:22 -0300

    Gerenciando tempo da lanterna

--- trabalho_final/Assets/Scripts/personagens/Personagem.cs
+++ trabalho_final/Assets/Scripts/personagens/Personagem.cs
@@ -1,28 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Personagem : MonoBehaviour
 {
     
     private float velocidadeDescolamento = 2f;
+    private int duracaoBateria = 5;
+
+    private int cargaBateria;
+    private float tempoUsoLanterna;
+    private float tempoLanternaPiscando;
+    private bool usandoLanterna;
+
+    public Text vlBateria;
+    public GameObject objLanterna;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        acenderLanterna();
     } 
 
     // Update is called once per frame
     void Update()
     {
-        float deslocX = Input.GetAxisRaw("Horizontal");
-        float addX = deslocX * velocidadeDescolamento * Time.deltaTime;
-        float novoX = transform.position.x + addX;
+        var deslocX = Input.GetAxisRaw("Horizontal");
+        var addX = deslocX * velocidadeDescolamento * Time.deltaTime;
+        var novoX = transform.position.x + addX;
         
-        float deslocY = Input.GetAxisRaw("Vertical");
-        float addY = deslocY * velocidadeDescolamento * Time.deltaTime;
-        float novoY = transform.position.y + addY;
+        var deslocY = Input.GetAxisRaw("Vertical");
+        var addY = deslocY * velocidadeDescolamento * Time.deltaTime;
+        var novoY = transform.position.y + addY;
 
         if (novoX > 9f || novoX < -9f)
         {
@@ -36,12 +47,48 @@ public class Personagem : MonoBehaviour
 
         transform.position = new Vector3(novoX, novoY, transform.position.z);
 
-        float rotatacaoZ = rotacao(transform.rotation.z, deslocX, deslocY);
+        var rotatacaoZ = rotacao(transform.rotation.z, deslocX, deslocY);
 
         if (transform.rotation.z != rotatacaoZ)
         {
             transform.eulerAngles = new Vector3(0, 0, rotatacaoZ);
         }
+
+        ///////////////////////////////////////////// Fim movimentação
+        
+        // Acendendo lanterna
+        if (Input.GetButtonDown("Jump")) { acenderLanterna();}
+        if (usandoLanterna) { gerenciarConsumoBateria(); }
+
+    }
+
+    void gerenciarConsumoBateria()
+    {
+        if (Time.time - tempoUsoLanterna >= 1f)
+        {
+            tempoUsoLanterna = Time.time;
+            cargaBateria-= 1;
+            vlBateria.text = cargaBateria + "s";
+        } else if (cargaBateria <= 1f && Time.time -tempoLanternaPiscando >= 0.15f)
+        {
+            tempoLanternaPiscando = Time.time;
+            objLanterna.SetActive( ! objLanterna.activeSelf );
+        }
+
+        if (cargaBateria <= 0)
+        {
+            objLanterna.SetActive(false);
+            usandoLanterna = false;
+        }
+    }
+
+    void acenderLanterna()
+    {
+        objLanterna.SetActive(true);
+        tempoUsoLanterna = Time.time;
+        usandoLanterna = true;
+        cargaBateria = 5;
+        tempoLanternaPiscando = 1;
     }
 
 
