ba01c1d4aed23af6158aa71e24bcff695071464e
Willian da Rosa <will.rosa.gvt@gmail.com>
2019-06-16 08:57:55 -0300

    Criando circulo editor

---
+++ trabalho_final/Assets/Scripts/personagens/FieldOfView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    
+    public float viewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+
+
+    public float y = 0f;
+    public float z = 0.7f;
+    public float x = 0.7f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
+        if (!angleIsGlobal) {
+            angleInDegrees += transform.eulerAngles.z;
+        }
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), z);
+        
+    }
+}
