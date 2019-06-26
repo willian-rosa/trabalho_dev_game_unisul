0bda54e3af0697e3634d130e5cf9633b4e83164e
Willian da Rosa <will.rosa.gvt@gmail.com>
2019-06-25 22:53:56 -0300

    Colocando o menu para baixo

--- trabalho_final/Assets/Scripts/personagens/Controle.cs
+++ trabalho_final/Assets/Scripts/personagens/Controle.cs
@@ -3,25 +3,26 @@ using System.Collections;
 
 public class Controle : MonoBehaviour {
 
-	private Camera viewCamera;
-	private float velocidadeDescolamento = 2f;
+    private Camera viewCamera;
+    private float velocidadeDescolamento = 2.0f;
+    public int velocidadeRotacao = 40;
 
-	void Start () {
-		viewCamera = Camera.main;
-	}
+    void Start () {
+        viewCamera = Camera.main;
+    }
 
-	void Update () {
-		var deslocX = Input.GetAxisRaw("Horizontal");
-		var addX = deslocX * velocidadeDescolamento * Time.deltaTime;
-		var novoX = transform.position.x + addX;
+    void Update () {
+        var deslocX = Input.GetAxisRaw("Horizontal");
+        var addX = deslocX * velocidadeDescolamento * Time.deltaTime;
+        var novoX = transform.position.x + addX;
         
-		var deslocY = Input.GetAxisRaw("Vertical");
-		var addY = deslocY * velocidadeDescolamento * Time.deltaTime;
-		var novoY = transform.position.y + addY;
+        var deslocY = Input.GetAxisRaw("Vertical");
+        var addY = deslocY * velocidadeDescolamento * Time.deltaTime;
+        var novoY = transform.position.y + addY;
 		
 		
-		transform.position = new Vector3(novoX, novoY, transform.position.z);
-		viewCamera.transform.position = new Vector3(novoX, novoY, viewCamera.transform.position.z);
+        transform.position = new Vector3(novoX, novoY, transform.position.z);
+        viewCamera.transform.position = new Vector3(novoX, novoY, viewCamera.transform.position.z);
 		
 		
 //		var rotatacaoZ = rotacao(transform.rotation.z, deslocX, deslocY);
@@ -32,53 +33,53 @@ public class Controle : MonoBehaviour {
 //        }
 		
 		
-		Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
-		Vector3 dir = Input.mousePosition - pos;
-		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition - pos;
+        float angle = Mathf.Atan2(dir.y, dir.x) * velocidadeRotacao;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 		
-	}
+    }
 	
 	
-	float rotacao(float rotatacaoZ, float deslocX, float deslocY)
-	{
-		if (deslocX == 1)
-		{
-			rotatacaoZ = 270;
-		} else if (deslocX == -1)
-		{
-			rotatacaoZ = 90;
-		}
+    float rotacao(float rotatacaoZ, float deslocX, float deslocY)
+    {
+        if (deslocX == 1)
+        {
+            rotatacaoZ = 270;
+        } else if (deslocX == -1)
+        {
+            rotatacaoZ = 90;
+        }
         
-		if (deslocY == 1)
-		{
-			if (deslocX == 1)
-			{
-				rotatacaoZ = 315;
-			} else if (deslocX == -1)
-			{
-				rotatacaoZ = 45;
-			}
-			else
-			{
-				rotatacaoZ = 0;
-			}
+        if (deslocY == 1)
+        {
+            if (deslocX == 1)
+            {
+                rotatacaoZ = 315;
+            } else if (deslocX == -1)
+            {
+                rotatacaoZ = 45;
+            }
+            else
+            {
+                rotatacaoZ = 0;
+            }
             
-		} else if (deslocY == -1)
-		{
-			if (deslocX == 1)
-			{
-				rotatacaoZ = 255;
-			} else if (deslocX == -1)
-			{
-				rotatacaoZ = 135;
-			}
-			else
-			{
-				rotatacaoZ = 180;
-			}
-		}
+        } else if (deslocY == -1)
+        {
+            if (deslocX == 1)
+            {
+                rotatacaoZ = 255;
+            } else if (deslocX == -1)
+            {
+                rotatacaoZ = 135;
+            }
+            else
+            {
+                rotatacaoZ = 180;
+            }
+        }
 
-		return rotatacaoZ;
-	}
+        return rotatacaoZ;
+    }
 }
